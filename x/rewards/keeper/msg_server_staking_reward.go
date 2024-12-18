14059aedbe2620e80dcf0f8f42c64ede2e766673
stefan.balea <stefan.balea@axsys.ro>
2024-12-19 00:53:14 +0200

    fixed staking reward counter

--- x/rewards/keeper/msg_server_staking_reward.go
+++ x/rewards/keeper/msg_server_staking_reward.go
@@ -56,6 +56,7 @@ func (k msgServer) CreateStakingReward(goCtx context.Context, msg *types.MsgCrea
 		ctx,
 		stakingReward,
 	)
+	k.incrementStakingRewardsCounter(ctx)
 
 	err = ctx.EventManager().EmitTypedEvent(
 		&types.StakingRewardCreateEvent{
