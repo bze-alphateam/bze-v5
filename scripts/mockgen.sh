3a1971271789eb85c5bf837c8c727bd226051369
stefan.balea <stefan.balea@axsys.ro>
2025-02-20 00:08:05 +0200

    added mocks for rewards module

--- scripts/mockgen.sh
+++ scripts/mockgen.sh
@@ -2,3 +2,4 @@
 
 mockgen_cmd="mockgen"
 $mockgen_cmd -source=x/tradebin/types/expected_keepers.go -package testutil -destination x/tradebin/testutil/expected_keepers_mocks.go
+$mockgen_cmd -source=x/rewards/types/expected_keepers.go -package testutil -destination x/rewards/testutil/expected_keepers_mocks.go
